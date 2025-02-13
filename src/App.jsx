db81ecb3f95a4a6b1c97a933f98bac93e3ff7a2b
Ana <anabfariasp@gmail.com>
2025-02-13 08:39:48 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
-import Agatha from "./assets/Agatha.png"
-import Ana from "./assets/Ana.png"
-import Zayra from "./assets/Zayra.png"
-import Anubis from "./assets/Anubis.png"
-import Lucas from "./assets/Lucas.png"
-import Juan from "./assets/Juan.png"
+import Header from "./components/Header/Header";
 import Logo from "./assets/logo.png";
+import Agatha from "./assets/Agatha.png";
+import Ana from "./assets/Ana.png";
+import Zayra from "./assets/Zayra.png";
+import Anubis from "./assets/Anubis.png";
+import Lucas from "./assets/Lucas.png";
+import Juan from "./assets/Juan.png";
 
 function App() {
   return (
     <>
-    
+      <Header desafio="DESAFIO 1 de PROPS - REACT" logo={Logo}/>
     </>
   );
 }
