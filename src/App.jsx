4e56d2f7b78525d109bd4b7c8c389168a2e575b4
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-02-13 08:20:24 -0300

    Meio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
-import Agatha from "./assets/Agatha.png"
-import Ana from "./assets/Ana.png"
-import Zayra from "./assets/Zayra.png"
-import Anubis from "./assets/Anubis.png"
-import Lucas from "./assets/Lucas.png"
-import Juan from "./assets/Juan.png"
+import Agatha from "./assets/Agatha.png";
+import Ana from "./assets/Ana.png";
+import Zayra from "./assets/Zayra.png";
+import Anubis from "./assets/Anubis.png";
+import Lucas from "./assets/Lucas.png";
+import Juan from "./assets/Juan.png";
+import Meio from "./components/Meio/Meio";
 
 function App() {
   return (
     <>
-    
+      <Meio nome="Juan Lopes" img={Juan} hobby="Hobby: Programar"/>
     </>
   );
 }
--- src/components/Meio/Meio.jsx
+++ src/components/Meio/Meio.jsx
@@ -3,10 +3,12 @@ import "./Meio.css";
 function Meio(props) {
   return (
     <>
-      <div className="card">
-        <p>{props.nome}</p>
-        <img src={props.img} alt="" className="imagem"/>
-        <p>{props.desc}</p>
+      <div className="container">
+        <div className="card">
+          <p className="nome">{props.nome}</p>
+          <img src={props.img} alt="" className="imagem" />
+          <p>{props.hobby}</p>
+        </div>
       </div>
     </>
   );
