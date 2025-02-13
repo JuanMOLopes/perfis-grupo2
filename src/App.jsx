d964322583abf2f0ff69e065d7d65a21b85762bb
Zayra <Zayrafranca5@gmail.com>
2025-02-13 08:29:10 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Agatha from "./assets/Agatha.png"
 import Ana from "./assets/Ana.png"
 import Zayra from "./assets/Zayra.png"
 import Anubis from "./assets/Anubis.png"
 import Lucas from "./assets/Lucas.png"
 import Juan from "./assets/Juan.png"
+import instagram from "./assets/insta.png"
+import gmail from "./assets/gmail.png"
 
 function App() {
   return (
     <>
-    
+   
+      <Footer texto1="Juan_Molopes.104🤳" insta={instagram} texto2="Juan.molopes@gmail.com 💻" gmail={gmail} texto3="O responsavel pelo desenvolvimento foi Juan Molopes.🎉"/>
+
     </>
   );
 }
