ab60f7b629eda2a949e6f2731b9ae190061f99b9
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-02-13 10:40:02 -0300

    fix css

--- src/components/Lado/Lado.jsx
+++ src/components/Lado/Lado.jsx
@@ -2,10 +2,10 @@ import "./Lado.css";
 
 function Lado(props) {
     return (
-        <div className="alinhamento">
-            <p>{props.texto1}</p>
-            <p>{props.texto2}</p>
-            <p>{props.texto3}</p>
+        <div className="lado">
+            <p className="valor">{props.txt1}</p>
+            <p className="valor">{props.txt2}</p>
+            <p className="valor">{props.txt3}</p>
         </div>
     );
 }
